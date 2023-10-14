f7fb01ea77cb234449fdbba43b43cac6910ace77
mamun-prodhan <mamunpr9@gmail.com>
2023-10-14 23:18:40 +0600

    user data displayed

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,13 @@ function App() {
     <>
       <h1>Users management system</h1>
       <h4>Number of users: {users.length}</h4>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.id}: {user.name} : {user.email}
+          </p>
+        ))}
+      </div>
     </>
   );
 }
